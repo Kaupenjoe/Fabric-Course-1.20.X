31e519c5e92258fa775d5b1380ff49e880085027
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-06-13 16:31:53 +0200

    custom item

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kaupenjoe.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,8 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// Very important Comment!
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
@@ -0,0 +1,33 @@
+package net.kaupenjoe.mccourse.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new FabricItemSettings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    private static void itemGroupIngredients(FabricItemGroupEntries entries) {
+        entries.add(PINK_GARNET);
+        entries.add(RAW_PINK_GARNET);
+    }
+
+    public static void registerModItems() {
+        MCCourseMod.LOGGER.info("Registering Mod Items for " + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::itemGroupIngredients);
+    }
+}
