ace5a0ead2e4f99ecff503edc4b169fe73ac0905
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-06-13 17:27:12 +0200

    item group

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -3,6 +3,7 @@ package net.kaupenjoe.mccourse;
 import net.fabricmc.api.ModInitializer;
 
 import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.kaupenjoe.mccourse.item.ModItemGroup;
 import net.kaupenjoe.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroup.registerItemGroups();
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
---
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItemGroup.java
@@ -0,0 +1,29 @@
+package net.kaupenjoe.mccourse.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup PINK_GARNET_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(MCCourseMod.MOD_ID, "pink_garnet_group"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.pink_garnet_group"))
+                    .icon(() -> new ItemStack(ModItems.PINK_GARNET)).entries((displayContext, entries) -> {
+                       entries.add(ModItems.PINK_GARNET);
+                       entries.add(ModItems.RAW_PINK_GARNET);
+
+                       entries.add(ModBlocks.PINK_GARNET_BLOCK);
+                       entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+                    }).build());
+
+    public static void registerItemGroups() {
+
+    }
+}
