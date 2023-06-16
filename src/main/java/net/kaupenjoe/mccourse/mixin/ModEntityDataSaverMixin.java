baeed8c6a34f690fe3b3881ec9e07b1d5ce131af
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-06-16 21:05:49 +0200

    mixin

---
+++ src/main/java/net/kaupenjoe/mccourse/mixin/ModEntityDataSaverMixin.java
@@ -0,0 +1,37 @@
+package net.kaupenjoe.mccourse.mixin;
+
+import net.kaupenjoe.mccourse.util.IEntityDataSaver;
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class ModEntityDataSaverMixin implements IEntityDataSaver {
+    private NbtCompound persistentData;
+
+    @Override
+    public NbtCompound getPersistentData() {
+        if(this.persistentData == null) {
+            this.persistentData = new NbtCompound();
+        }
+        return persistentData;
+    }
+
+    @Inject(method = "writeNbt", at = @At("HEAD"))
+    protected void injectWriteMethod(NbtCompound nbt, CallbackInfoReturnable<NbtCompound> info) {
+        if(this.persistentData != null) {
+            nbt.put("mccourse.custom_data", persistentData);
+        }
+    }
+
+    @Inject(method = "readNbt", at = @At("HEAD"))
+    protected void injectReadMethod(NbtCompound nbt, CallbackInfo info) {
+        if(nbt.contains("mccourse.custom_data", 10)) {
+            this.persistentData = nbt.getCompound("mccourse.custom_data");
+        }
+    }
+}
