17c9500f7a12f7089bbbd61872881da1e60741a0
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-06-16 17:18:43 +0200

    custom composting

--- src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
+++ src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
@@ -2,15 +2,22 @@ package net.kaupenjoe.mccourse.util;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.item.ModItems;
+import net.minecraft.block.ComposterBlock;
 
 public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
+        registerModCompostables();
     }
 
     private static void registerFuels() {
         FuelRegistry registry = FuelRegistry.INSTANCE;
 
         registry.add(ModItems.PEAT_BRICK, 200);
+    }
+
+    private static void registerModCompostables() {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER, 0.5f);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER_SEEDS, 0.25f);
     }
 }
