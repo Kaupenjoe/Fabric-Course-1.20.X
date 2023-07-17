463c3cad51c70ee1c63a22b870bf1b04b0d864d8
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-07-17 20:36:32 +0200

    entity spawns

---
+++ src/main/java/net/kaupenjoe/mccourse/world/gen/ModEntitySpawns.java
@@ -0,0 +1,20 @@
+package net.kaupenjoe.mccourse.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.kaupenjoe.mccourse.entity.ModEntities;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.BiomeKeys;
+
+public class ModEntitySpawns {
+    public static void addSpawns() {
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.PLAINS, BiomeKeys.FOREST),
+                SpawnGroup.CREATURE, ModEntities.PORCUPINE, 50, 1, 3);
+
+        SpawnRestriction.register(ModEntities.PORCUPINE, SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+    }
+}
--- src/main/java/net/kaupenjoe/mccourse/world/gen/ModWorldGeneration.java
+++ src/main/java/net/kaupenjoe/mccourse/world/gen/ModWorldGeneration.java
@@ -6,5 +6,7 @@ public class ModWorldGeneration {
 
         ModTreeGeneration.generateTrees();
         ModFlowerGeneration.generateFlowers();
+
+        ModEntitySpawns.addSpawns();
     }
 }
